9fbb93bfcea767c83f1474f34427644667fc2c3e
Saul Ceja <saulceja08@gmail.com>
2023-07-16 13:32:40 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,23 +1,114 @@
-/* Your Code Here */
+const chai = require('chai');
+const spies = require('chai-spies');
+chai.use(spies);
+const expect = chai.expect;
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
+// Helper functions
+function createEmployeeRecord(employeeData) {
+  const [firstName, familyName, title, payPerHour] = employeeData;
+  return {
+    firstName: firstName,
+    familyName: familyName,
+    title: title,
+    payPerHour: payPerHour,
+    timeInEvents: [],
+    timeOutEvents: []
+  };
 }
 
+function createEmployeeRecords(employeesData) {
+  return employeesData.map(employeeData => createEmployeeRecord(employeeData));
+}
+
+function createTimeInEvent(dateStamp) {
+  if (!dateStamp || typeof dateStamp !== "string") {
+    throw new Error("Invalid dateStamp");
+  }
+
+  const [date, hour] = dateStamp.split(" ");
+
+  this.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date: date
+  });
+
+  return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+  if (!dateStamp || typeof dateStamp !== "string") {
+    throw new Error("Invalid dateStamp");
+  }
+
+  const [date, hour] = dateStamp.split(" ");
+
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date: date
+  });
+
+  return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const timeInEvent = this.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = this.timeOutEvents.find(event => event.date === date);
+  
+    if (timeInEvent && timeOutEvent) {
+      const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100; // Convert to hours
+      return hoursWorked;
+    }
+  
+    return 0;
+  }
+  
+
+function wagesEarnedOnDate(date) {
+  const hoursWorked = hoursWorkedOnDate.call(this, date);
+  const payRate = this.payPerHour;
+  const wagesEarned = hoursWorked * payRate;
+
+  return wagesEarned;
+}
+
+function allWagesFor() {
+  const datesWorked = this.timeInEvents.map(event => event.date);
+  const totalWages = datesWorked.reduce(
+    (total, date) => total + wagesEarnedOnDate.call(this, date),
+    0
+  );
+
+  return totalWages;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+  return srcArray.find(employee => employee.firstName === firstName);
+}
+
+function calculatePayroll(employeeRecords) {
+  return employeeRecords.reduce(
+    (totalPayroll, employeeRecord) =>
+      totalPayroll + allWagesFor.call(employeeRecord),
+    0
+  );
+}
+
+// Test cases
+describe("The payroll system", function () {
+  // Rest of the test cases
+});
+
+// Export the functions to be used in the test cases
+module.exports = {
+  createEmployeeRecord,
+  createEmployeeRecords,
+  createTimeInEvent,
+  createTimeOutEvent,
+  hoursWorkedOnDate,
+  wagesEarnedOnDate,
+  allWagesFor,
+  findEmployeeByFirstName,
+  calculatePayroll
+};
